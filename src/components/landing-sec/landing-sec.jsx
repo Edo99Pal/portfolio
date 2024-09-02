84d14d5baa602c849423ce3fed52773ddc9b86c6
Edo99Pal <56258699+Edo99Pal@users.noreply.github.com>
2024-09-02 03:22:14 +0100

    Update

--- src/components/landing-sec/landing-sec.jsx
+++ src/components/landing-sec/landing-sec.jsx
@@ -3,16 +3,13 @@ import './landing-sec.scss';
 function LandingPage() {
     return(
         <section id="cv">
-            <div className="left">
+            <div id="bio">
                 <h2>CV</h2>
                 <p>Edoardo Palamito is a skilled web and fullstack developer with a strong background in technology and innovation. He completed his studies at ITI Guglielmo Marconi in Catania, where he laid the foundation for his expertise in the tech field.
 
 Following his education, Edoardo embarked on his professional journey in Catania, contributing his skills to two notable companies. He began his career at Emmé Pubblicità, where he honed his skills as a web developer, focusing on creating dynamic and user-friendly web solutions. Later, he joined Sapegis Innovazione as a fullstack developer, where he expanded his expertise to both front-end and back-end development, working on comprehensive projects that blend innovation with practicality.
 
 Edoardo's career is marked by a commitment to technological excellence and a passion for developing impactful digital solutions.</p>
-            </div>
-            <div className="right">
-                <img src="images/propic.png" alt="Edoardo Palamito"/>
             </div>
         </section>
     )
